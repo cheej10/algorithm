7ec58fc58053d3bcc04c4a86e7d5db7ce4072d07
cheej10 <65033180+cheej10@users.noreply.github.com>
2021-10-21 21:06:40 +0900

    Create 1110_더하기 사이클.js

---
+++ baekjoon/array/1110_더하기 사이클.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString();
+
+let newNum = Number(input);
+    let answer = 0;
+    do {
+        let ten = Math.floor(newNum / 10);
+        let one = newNum % 10;
+        let sum = ten + one;
+        newNum = one * 10 + sum % 10;
+        answer++;
+    } while (newNum !== Number(input));
+
+console.log(answer);
